3622691ed646a5653f71de0700e20d0549e55f53
Moviesuploader <138097021+Moviesuploader@users.noreply.github.com>
2024-04-15 12:24:27 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -2063,7 +2063,7 @@ async def auto_filter(client, msg, spoll=False):
             return
         if len(message.text) < 100:
             search = message.text
-            m=await message.reply_sticker("CAACAgUAAxkBAAIVsGX0kzPp94CVhWSK89_ru6SSo2m6AAJdDQAC4lzxV8kUd8y6FlxoNAQ",
+            m=await message.reply_sticker("CAACAgQAAxkBAAIZBmYczm5S-NWE3EjVZNYWPQ3GjoHhAALUAgACcnEXJVQhiQMcUCh8NAQ",
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f'Seaching for {search} 🔎', url=f"https://t.me/beautyofthemoviesx")]]) 
             )
             search = search.lower()
@@ -2094,7 +2094,7 @@ async def auto_filter(client, msg, spoll=False):
     else:
         message = msg.message.reply_to_message  # msg will be callback query
         search, files, offset, total_results = spoll
-        m=await message.reply_sticker("CAACAgUAAxkBAAIVsGX0kzPp94CVhWSK89_ru6SSo2m6AAJdDQAC4lzxV8kUd8y6FlxoNAQ",                              
+        m=await message.reply_sticker("CAACAgQAAxkBAAIZBmYczm5S-NWE3EjVZNYWPQ3GjoHhAALUAgACcnEXJVQhiQMcUCh8NAQ",                              
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f'Seaching for {search} 🔎', url=f"https://t.me/beautyofthemoviesx")]]) 
         )
         settings = await get_settings(message.chat.id)
